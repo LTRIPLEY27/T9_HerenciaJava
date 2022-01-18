3f44c53cf04d324d6fa581259187e431758ba3b6
Skice7 <55830756+Skice7@users.noreply.github.com>
2022-01-18 10:14:44 +0100

    commit

---
+++ src/ejercicio6/Main.java
@@ -0,0 +1,16 @@
+package ejercicio6;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		peliculas pelicula1 = new peliculas("a",1,"b");
+		
+		
+		System.out.println(pelicula1.toString());
+
+	}
+
+}
---
+++ src/ejercicio6/peliculas.java
@@ -0,0 +1,25 @@
+package ejercicio6;
+
+public class peliculas {
+
+	
+	//valores
+	protected String titulo;
+	protected double duracion;
+	protected String director;
+	
+	//Constructor con parametros
+	public peliculas(String titulo, double duracion, String director) {
+		super();
+		this.titulo = titulo;
+		this.duracion = duracion;
+		this.director = director;
+	}
+
+	public String toString() {
+		return "peliculas [titulo=" + titulo + ", duracion=" + duracion + ", director=" + director + "]";
+	}
+	
+	
+	
+}
