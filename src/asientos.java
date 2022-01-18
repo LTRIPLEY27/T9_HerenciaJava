9f3c4dfc6cf30f816e0019f3eac5c0e536ecba3a
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-01-18 11:17:03 +0100

    No acabado, falta el boolean

--- src/asientos.java
+++ src/asientos.java
@@ -17,16 +17,22 @@ public class asientos {
 			x--;
 			//Vovlemos a devolver 64 (A en ASCII) para que a la siguiente fila empiece por A
 			y = 64;
+			//Convertimos la variable x en string para insertarlo en el array
 			String silla=String.valueOf(x);
-			for (int j=0;j<asiento.length+1;j++) {
-				y++;
-				char sillaNum= (char)y;
-				String sillaString=String.valueOf(sillaNum);
-				asiento[i][j]=silla;
-				System.out.print(asiento[i][j]+" ");
-				asiento[i][j]= sillaString;
-				System.out.print(asiento[i][j]+" ");
-			}
+				for (int j=0;j<asiento.length+1;j++) {
+					//Vamos sumando la y + 1 para cambiar de letras
+					y++;
+					//Convertimos el numero en char para ver la letra en ASCII
+					char sillaNum= (char)y;
+					//Convertimos char a string para aplicarlo al array
+					String sillaString=String.valueOf(sillaNum);
+					//Asignamos el numero del asiento, empezara por 8, acabara en 1
+					asiento[i][j]=silla;
+					System.out.print(asiento[i][j]+" ");
+					//Asignamos la letra en la posicion exacta de la silla, empieza por A, acaba en I
+					asiento[i][j]= sillaString;
+					System.out.print(asiento[i][j]+" ");
+				}
 			System.out.println("\n");
 		}
 	}
